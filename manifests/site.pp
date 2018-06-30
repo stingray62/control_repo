13b05623dd1486d2b1d9d38fbb460b42cda32505
stingray62 <matt@mhsbits.com>
2018-06-30 17:55:42 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,3 +7,11 @@ node 'pmaster.shade.local' {
 node 'cboxes.shade.local' {
   include role::master_server
 }
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
+}
